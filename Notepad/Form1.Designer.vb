a859ce1736d73ab69cab47c5105ed2be66c7616e
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-09-14 15:24:18 +0700

    fix: edit dropdown toool strip menu

--- Notepad/Form1.Designer.vb
+++ Notepad/Form1.Designer.vb
@@ -47,8 +47,10 @@ Partial Class Form1
         Me.OpenToolStrip = New ToolStripMenuItem()
         Me.SaveToolStrip = New ToolStripMenuItem()
         Me.SaveAsToolStrip = New ToolStripMenuItem()
+        Me.BottomSaveAsSeparator = New ToolStripSeparator()
         Me.PageSetupToolStrip = New ToolStripMenuItem()
         Me.PrintToolStrip = New ToolStripMenuItem()
+        Me.BottomPrintSeparator = New ToolStripSeparator()
         Me.ExitToolStrip = New ToolStripMenuItem()
 
         Me.NewToolStrip.Text = "New"
@@ -67,39 +69,67 @@ Partial Class Form1
             Me.OpenToolStrip,
             Me.SaveToolStrip,
             Me.SaveAsToolStrip,
+            Me.BottomSaveAsSeparator,
             Me.PageSetupToolStrip,
             Me.PrintToolStrip,
+            Me.BottomPrintSeparator,
             Me.ExitToolStrip
         })
 
         'Edit
         Me.UndoToolStrip = New ToolStripMenuItem()
+        Me.BottomUndoSeparator = New ToolStripSeparator()
         Me.CutToolStrip = New ToolStripMenuItem()
         Me.CopyToolStrip = New ToolStripMenuItem()
         Me.PasteToolStrip = New ToolStripMenuItem()
         Me.DeleteToolStrip = New ToolStripMenuItem()
+        Me.BottomDeleteSeparator = New ToolStripSeparator()
         Me.SearchBingToolStrip = New ToolStripMenuItem()
-        Me.FileToolStrip = New ToolStripMenuItem()
+        Me.FindToolStrip = New ToolStripMenuItem()
         Me.FindNextToolStrip = New ToolStripMenuItem()
         Me.FindPreviousToolStrip = New ToolStripMenuItem()
         Me.ReplaceToolStrip = New ToolStripMenuItem()
         Me.GotoToolStrip = New ToolStripMenuItem()
+        Me.BottomGotoSeparator = New ToolStripSeparator()
         Me.SearchAllToolStrip = New ToolStripMenuItem()
         Me.TimeDateToolStrip = New ToolStripMenuItem()
+
+        Me.UndoToolStrip.Text = "Undo"
+        Me.UndoToolStrip.Enabled = False
+        Me.CutToolStrip.Text = "Cut"
+        Me.CutToolStrip.Enabled = False
+        Me.CopyToolStrip.Text = "Copy"
+        Me.CopyToolStrip.Enabled = False
+        Me.PasteToolStrip.Text = "Paste"
+        Me.DeleteToolStrip.Text = "Delete"
+        Me.SearchBingToolStrip.Text = "Search With Bing..."
+        Me.FindToolStrip.Text = "Find..."
+        Me.FindToolStrip.Enabled = False
+        Me.FindNextToolStrip.Text = "Find Next"
+        Me.FindNextToolStrip.Enabled = False
+        Me.FindPreviousToolStrip.Text = "Find Previous"
+        Me.FindPreviousToolStrip.Enabled = False
+        Me.ReplaceToolStrip.Text = "Replace..."
+        Me.GotoToolStrip.Text = "Go To..."
+        Me.SearchAllToolStrip.Text = "Search All..."
+        Me.TimeDateToolStrip.Text = "Time/Date"
 
         Me.EditToolStrip.Text = "Edit"
         Me.EditToolStrip.DropDownItems.AddRange(New ToolStripItem() {
             Me.UndoToolStrip,
+            Me.BottomUndoSeparator,
             Me.CutToolStrip,
             Me.CopyToolStrip,
             Me.PasteToolStrip,
             Me.DeleteToolStrip,
+            Me.BottomDeleteSeparator,
             Me.SearchBingToolStrip,
-            Me.FileToolStrip,
+            Me.FindToolStrip,
             Me.FindNextToolStrip,
             Me.FindPreviousToolStrip,
             Me.ReplaceToolStrip,
             Me.GotoToolStrip,
+            Me.BottomGotoSeparator,
             Me.SearchAllToolStrip,
             Me.TimeDateToolStrip
         })
@@ -147,22 +177,27 @@ Partial Class Form1
     Friend WithEvents OpenToolStrip As ToolStripMenuItem
     Friend WithEvents SaveToolStrip As ToolStripMenuItem
     Friend WithEvents SaveAsToolStrip As ToolStripMenuItem
+    Friend WithEvents BottomSaveAsSeparator As ToolStripSeparator
     Friend WithEvents PageSetupToolStrip As ToolStripMenuItem
     Friend WithEvents PrintToolStrip As ToolStripMenuItem
+    Friend WithEvents BottomPrintSeparator As ToolStripSeparator
     Friend WithEvents ExitToolStrip As ToolStripMenuItem
 
     'EditToolStrip
     Friend WithEvents UndoToolStrip As ToolStripMenuItem
+    Friend WithEvents BottomUndoSeparator As ToolStripSeparator
     Friend WithEvents CutToolStrip As ToolStripMenuItem
     Friend WithEvents CopyToolStrip As ToolStripMenuItem
     Friend WithEvents PasteToolStrip As ToolStripMenuItem
     Friend WithEvents DeleteToolStrip As ToolStripMenuItem
+    Friend WithEvents BottomDeleteSeparator As ToolStripSeparator
     Friend WithEvents SearchBingToolStrip As ToolStripMenuItem
     Friend WithEvents FindToolStrip As ToolStripMenuItem
     Friend WithEvents FindNextToolStrip As ToolStripMenuItem
     Friend WithEvents FindPreviousToolStrip As ToolStripMenuItem
     Friend WithEvents ReplaceToolStrip As ToolStripMenuItem
     Friend WithEvents GotoToolStrip As ToolStripMenuItem
+    Friend WithEvents BottomGotoSeparator As ToolStripSeparator
     Friend WithEvents SearchAllToolStrip As ToolStripMenuItem
     Friend WithEvents TimeDateToolStrip As ToolStripMenuItem
 
