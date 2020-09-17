faf9b0e7be690c0b3f2f82c991954653e4e0cc41
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-09-17 21:30:03 +0700

    add: save file

--- Notepad/Form1.vb
+++ Notepad/Form1.vb
@@ -1,7 +1,11 @@
 ﻿Public Class Form1
+    Friend IsNewFile As Boolean
+    Friend FilePath As String
+
     Public Sub New()
         InitializeController()
         InitializeComponent()
+        IsNewFile = True
     End Sub
 
 #Region "FileToolStrip"
@@ -13,19 +17,45 @@
         Dim result As DialogResult = FileDlg.ShowDialog()
         If result = DialogResult.OK Then
             ChangeFormText(FileDlg.SafeFileName)
-            Dim fileData As String = WorkWithFile.ReadFile(FileDlg.FileName)
+            FilePath = FileDlg.FileName
+            Dim fileData As String = WorkWithFile.ReadFile(FilePath)
             textArea.Text = fileData
+            IsNewFile = False
         End If
+    End Sub
+
+    Private Sub SaveToolStrip_Click(sender As Object, e As EventArgs) Handles SaveToolStrip.Click
+        If IsNewFile Then
+            Dim result As DialogResult = SaveFileDlg.ShowDialog()
+            If result = DialogResult.OK Then
+                Dim check As Boolean = WorkWithFile.WriteNewFile(SaveFileDlg.FileName, textArea.Text)
+                If Not check Then
+                    MsgBox("Do Not Save File")
+                    Return
+                End If
+                ChangeFormText(SaveFileDlg.FileName)
+                IsNewFile = False
+            End If
+        Else
+
+        End If
+
+
     End Sub
 
     Private Sub SaveAsToolStrip_Click(sender As Object, e As EventArgs) Handles SaveAsToolStrip.Click
         Dim result As DialogResult = SaveFileDlg.ShowDialog()
         If result = DialogResult.OK Then
             Dim check As Boolean = WorkWithFile.WriteNewFile(SaveFileDlg.FileName, textArea.Text)
+            MsgBox(SaveFileDlg.FileNames)
             If Not check Then
-                MsgBox("Do not save file")
+                MsgBox("Do Not Save File")
             End If
         End If
+    End Sub
+
+    Private Sub TextArea_KeyPress(sender As Object, e As KeyPressEventArgs) Handles textArea.KeyPress
+        ChangeFormText("*" & Me.FileName)
     End Sub
 #End Region
 End Class
