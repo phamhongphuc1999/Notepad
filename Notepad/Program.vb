25c8a76762d3a928075d3db24df0b6dcbd8e2118
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-09-20 19:09:01 +0700

    add: custom main function

---
+++ Notepad/Program.vb
@@ -0,0 +1,7 @@
+﻿Public Class Program
+    Public Shared Sub Main()
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New PageSetupForm)
+    End Sub
+End Class
